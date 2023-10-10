f72f3c150af7c79f99a492571d303d21f9fab900
Thomas Randall <tlranda@clemson.edu>
2023-10-10 17:35:33 -0400

    Debugging, polling, and shutdown
    NEW:
    * Enum for debug levels and initial setup to guard output behind debug levels
    * Duration for polling implemented; proper runtime loop
    * Interrupt handling via shutdown()
    
    Modified:
    * Arguments are global scope to permit usage in shutdown()
    * Constant string for getopt argument handling
    * Better argument parsing/error diagnostics when arguments are misunderstood
    
    Cleanup and Clarified:
    * Includes (and document why they are included)
    * Constant for string buffers
    * Better whitespace for readability
    * Note from Ben regarding NVML unit functions

--- sensor_tool.cpp
+++ sensor_tool.cpp
@@ -1,38 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <iomanip>
-#include <cstring>
-#include <chrono>
-#include <thread>
+#include <iostream> // cout, cerr, etc
+#include <iomanip> // setw and setprecision
+#include <thread> // Often implies <chrono>
+#include <chrono> // Given explicitly in case of above comment being untrue
+
 #include <unistd.h> // Getopt
 #include <getopt.h> // Getopt-long
-#include <cstdlib>
+#include <signal.h> // signal interrupts
 #include <sensors/sensors.h> // Must compile with -lsensors
 
 #include <cuda.h> // Must compile with -lcuda
 #include <cuda_runtime.h> // Must compile with -lcudart
 #include <nvml.h> // Must compile with -lnvidia-ml
 
+// safecuda includes <cuda.h>, <cuda_runtime.h>, <nvrtc.h>, <stdlib.h>, <stdio.h>, <cublas_v2.h>
 #include "safecuda.h" // Macros to do safe cuda calls
 
-#define CHIP_NAME_BUFFER_SIZE 200
+// LibSensors and NVML demos typically allocate this much space for chip/driver names
+#define NAME_BUFFER_SIZE 200
+
+// Debug levels as enum to automatically count the range of argument values
+enum DebugLevels {
+DebugOFF,
+DebugMinimal,
+DebugVerbose
+};
 
 // Argument values stored here
 typedef struct argstruct {
     short help = 0,
           cpu = 0,
-          gpu = 0;
+          gpu = 0,
+          debug = 0;
     char* log = 0;
     double poll = 0.;
+    std::chrono::duration<double> duration;
 } arguments;
+// Arguments have global scope to permit reference during shutdown (debug values, file I/O, etc)
+arguments args;
+
+
 
 /* Read command line and parse arguments
    Pointer args used to store semantic settings for program execution
  */
-void parse(int argc, char** argv, arguments* args) {
+void parse(int argc, char** argv) {
     char* PROGNAME = argv[0];
-    int c;
+    int c, bad_args = 0;
 
     // Getopt option declarations
     static struct option long_options[] = {
@@ -41,21 +54,24 @@ void parse(int argc, char** argv, arguments* args) {
         {"gpu", no_argument, 0, 'g'},
         {"log", required_argument, 0, 'l'},
         {"poll", required_argument, 0, 'p'},
+        {"debug", required_argument, 0, 'd'},
         {0,0,0,0}
     };
-    opterr = 0; // Disable getopt's automatic error message -- we'll catch it via the '?' return and shut down
+    const char* optionstr = "hcgl:p:d:";
+    // Disable getopt's automatic error message -- we'll catch it via the '?' return and shut down
+    opterr = 0;
 
     // Parsing loop
     while (1) {
-        int this_option_optind = optind ? optind : 1;
         int option_index = 0;
-        c = getopt_long(argc, argv, "hcgl:p:", long_options, &option_index);
+        c = getopt_long(argc, argv, optionstr, long_options, &option_index);
         if (c == -1) break;
         switch (c) {
+            // This case may be deprecated -- blindly following a demo that includes it for some reason
             case 0:
-                printf("Weird option %s", long_options[option_index].name);
-                if (optarg) printf(" with arg %s", optarg);
-                printf("\n");
+                std::cerr << "Weird option " << long_options[option_index].name;
+                if (optarg) std::cerr << " with arg " << optarg;
+                std::cerr << std::endl;
                 break;
             case 'h':
                 std::cout << "Usage: " << PROGNAME << " [options]" << std::endl;
@@ -69,40 +85,74 @@ void parse(int argc, char** argv, arguments* args) {
                              "File to write output to" << std::endl;
                 std::cout << "\t-p [interval] | --poll [interval]\n\t\t" <<
                              "Floating point interval in seconds to poll stats (interval > 0)" << std::endl;
+                std::cout << "\t-d [level] | --debug [level]\n\t\t" <<
+                             "Debug verbosity (default: 0, maximum: 1)" << std::endl;
                 exit(EXIT_SUCCESS);
             case 'c':
-                args->cpu = 1;
+                args.cpu = 1;
                 break;
             case 'g':
-                args->gpu = 1;
+                args.gpu = 1;
                 break;
             case 'l':
-                args->log = optarg;
+                args.log = optarg;
                 break;
             case 'p':
-                args->poll = atof(optarg);
+                args.poll = atof(optarg);
+                if (args.poll < 0) {
+                    std::cerr << "Invalid setting for " << argv[optind-2] << ": " << optarg <<
+                                 "\n\tPolling duration must be greater than 0" << std::endl;
+                    bad_args += 1;
+                }
+                else {
+                    // Set sleep time for nanosleep
+                    args.duration = static_cast<std::chrono::duration<double>>(args.poll);
+                }
+                break;
+            case 'd':
+                args.debug = atoi(optarg);
+                if (args.debug < DebugOFF || args.debug > DebugVerbose) { // Invalid debug levels
+                    std::cerr << "Invalid setting for " << argv[optind-2] << ": " << optarg <<
+                                 "\n\tValid levels are integers between " << DebugOFF <<
+                                 " and " << DebugVerbose << std::endl;
+                    bad_args += 1;
+                }
                 break;
             case '?':
                 std::cerr << "Unrecognized argument: " << argv[optind-1] << std::endl;
-                exit(EXIT_FAILURE);
+                bad_args += 1;
                 break;
         }
     }
     if (optind < argc) {
-        std::cerr << "Unrecognized additional arguments!" << std::endl;
+        std::cerr << "Unrecognized additional arguments!" << std::endl << "\t";
+        for (int i = optind; i < argc; i++) {
+            std::cerr << argv[i] << " ";
+        }
+        std::cerr << std::endl;
+        bad_args += 1;
+    }
+
+    // Post-reading logic
+    if (!args.cpu && !args.gpu) { // Default: Handle both CPU and GPU
+        args.cpu = 1;
+        args.gpu = 1;
+    }
+    if (bad_args > 0) {
         exit(EXIT_FAILURE);
     }
 }
 
+
 void collect_cpu(void) {
 	// Try to fetch chips?
 	int nr = 0;
-	char chip_name[CHIP_NAME_BUFFER_SIZE];
+	char chip_name[NAME_BUFFER_SIZE];
 	auto name = sensors_get_detected_chips(nullptr, &nr);
 	while (name) {
 		// Clear chip name buffer
-        memset(chip_name, 0, CHIP_NAME_BUFFER_SIZE);
-        sensors_snprintf_chip_name(chip_name, CHIP_NAME_BUFFER_SIZE, name);
+        memset(chip_name, 0, NAME_BUFFER_SIZE);
+        sensors_snprintf_chip_name(chip_name, NAME_BUFFER_SIZE, name);
 		if (strcmp(chip_name, "k10temp-pci-00cb") != 0) { // DEBUG ONLY: Limit output to single chip
 			name = sensors_get_detected_chips(nullptr, &nr);
 			continue;
@@ -158,6 +208,7 @@ void collect_cpu(void) {
 	}
 }
 
+
 typedef struct stats_ {
     std::time_t timestamp;
     uint temperature;
@@ -167,6 +218,8 @@ typedef struct stats_ {
     nvmlMemory_t memory; // ull free | ull total | ull used (all bytes)
     nvmlPstates_t performanceState;
 } stats;
+
+
 
 void collect_gpu(void) {
     int n_devices;
@@ -183,6 +236,7 @@ void collect_gpu(void) {
         std::cout << "Device " << i << " Temperature: " << temperature << std::endl;
 
         // NVML Unit?
+        // BEN: Units only supported by S-class cards; we're not using those so any nvmlUnit* calls don't work
         nvmlUnit_t unit;
         nvmlUnitGetHandleByIndex(static_cast<unsigned int>(i), &unit);
         nvmlUnitGetTemperature(unit, 0, &temperature);
@@ -216,48 +270,79 @@ void collect_gpu(void) {
     */
 }
 
+
+// Cleanup calls should be based on globally available information; process-killing interrupts will go through this function
+void shutdown(int s = 0) {
+    if (args.debug >= DebugMinimal) std::cerr << "Run shutdown with signal " << s << std::endl;
+    nvmlShutdown();
+    sensors_cleanup();
+    if (args.debug >= DebugMinimal) std::cerr << "Shutdown clean. Exiting..." << std::endl;
+    exit(EXIT_SUCCESS);
+}
+
+
 int main(int argc, char** argv) {
-	arguments args;
-    parse(argc, argv, &args);
-    std::cout << "The program lives" << std::endl;
-    std::cout << "Arguments evaluate to" << std::endl;
-    std::cout << "Help: " << args.help << std::endl;
-    std::cout << "CPU: " << args.cpu << std::endl;
-    std::cout << "GPU: " << args.gpu << std::endl;
-    if (args.log == 0) {
-        std::cout << "Log: NONE" << std::endl;
-    } else {
-        std::cout << "Log: " << args.log << std::endl;
-    }
-    std::cout << "Poll: " << args.poll << std::endl;
-    //exit(EXIT_SUCCESS);
+    // Library initializations
 	auto const error = sensors_init(NULL);
 	if(error != 0) {
 		std::cerr << "LibSensors library did not initialize properly! Aborting..." << std::endl;
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
     nvmlInit();
 
-    // TODO: Command line argument parsing
+    // Prepare shutdown via CTRL+C or other signals
+    struct sigaction sigHandler;
+    sigHandler.sa_handler = shutdown;
+    sigemptyset(&sigHandler.sa_mask);
+    sigHandler.sa_flags = 0;
+    sigaction(SIGINT, &sigHandler, NULL);
+    sigaction(SIGABRT, &sigHandler, NULL);
+    sigaction(SIGTERM, &sigHandler, NULL);
+    // SIGKILL and SIGSTOP cannot be caught, blocked, or ignored
 
-    // TODO: Guard this output behind argument to check versions
-	// Fetch library version
-	//std::cout << "Using libsensors v" << libsensors_version << std::endl;
-    int NVML_VERSION;
-    nvmlSystemGetCudaDriverVersion(&NVML_VERSION);
-    char NVML_DRIVER_VERSION[200];
-    nvmlSystemGetDriverVersion(NVML_DRIVER_VERSION, 200);
-    std::cout << "Using NVML v" << NVML_VERSION << std::endl;
-    std::cout << "Using NVML v" << NVML_DRIVER_VERSION << std::endl;
-    //collect_cpu();
-    collect_gpu();
+    // Command line argument parsing
+    parse(argc, argv);
+    if (args.debug >= DebugVerbose) {
+        std::cout << "The program lives" << std::endl;
+    }
+    if (args.debug >= DebugMinimal) {
+        std::cout << "Arguments evaluate to" << std::endl <<
+        "Help: " << args.help << std::endl <<
+        "CPU: " << args.cpu << std::endl <<
+        "GPU: " << args.gpu << std::endl;
+        if (args.log == 0) std::cout << "Log: --" << std::endl;
+        else std::cout << "Log: " << args.log << std::endl;
+        std::cout << "Poll: " << args.poll << std::endl <<
+        "Debug: " << args.debug << std::endl;
+    }
 
-    // std::time_t timestamp = std::chrono::high_resolution-clock::now().time_since_epoch().count();
-    // std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    // Denote library versions
+    if (args.debug >= DebugVerbose) {
+        int NVML_VERSION;
+        char NVML_DRIVER_VERSION[NAME_BUFFER_SIZE];
+        nvmlSystemGetCudaDriverVersion(&NVML_VERSION);
+        nvmlSystemGetDriverVersion(NVML_DRIVER_VERSION, NAME_BUFFER_SIZE);
+        std::cout << "Using libsensors v" << libsensors_version << std::endl;
+        std::cout << "Using NVML v" << NVML_VERSION << std::endl;
+        std::cout << "Using NVML Driver v" << NVML_DRIVER_VERSION << std::endl;
+    }
 
-	std::cout << "The program ends" << std::endl;
+    // Main Loop
+    while (1) {
+        // Collection
+        if (args.cpu) collect_cpu();
+        if (args.gpu) collect_gpu();
+        // Sleeping between polls
+        if (args.poll == 0) break;
+        else std::this_thread::sleep_for(args.duration);
+    }
 
-    nvmlShutdown();
-	return 0;
+    if (args.debug >= DebugVerbose) {
+        std::cout << "The program ends" << std::endl;
+    }
+
+    // Shutdown
+    shutdown();
+    return 0;
 }
 
